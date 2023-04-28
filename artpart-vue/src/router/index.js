e0a82a188f8fe0f828e1273835dae546d8a0af7c
nakhyun <skrgus0140@naver.com>
2023-04-28 15:45:30 +0900

    충돌수정

--- artpart-vue/src/router/index.js
+++ artpart-vue/src/router/index.js
@@ -26,26 +26,26 @@ import MemberMain from '@/views/member/MemberMain.vue'
 
 
 
-{
-  path: '/Pay',
-  name: 'ManageMentPay',
-  component: ManageMentPay
-},
-//관리비 사용 내역
-{
-  path: '/Using',
-  name: 'ManageMentUsing',
-  component: ManageMentUsing
-},
-//시설 관리
-{
-  path: '/Facility',
-  name: 'FacilityManageMent',
-  component: FacilityManageMent
-}
 
 const routes = [
 
+  {
+    path: '/Pay',
+    name: 'ManageMentPay',
+    component: ManageMentPay
+  },
+  //관리비 사용 내역
+  {
+    path: '/Using',
+    name: 'ManageMentUsing',
+    component: ManageMentUsing
+  },
+  //시설 관리
+  {
+    path: '/Facility',
+    name: 'FacilityManageMent',
+    component: FacilityManageMent
+  },
   //----------------------------------------------
   //인트로
   {
