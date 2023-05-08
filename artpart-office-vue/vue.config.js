02a97348f51b2d05ab3e1da03227118c8a9f54bd
nakhyun <skrgus0140@naver.com>
2023-05-08 11:08:44 +0900

    vue.config output dir 설정

--- artpart-office-vue/vue.config.js
+++ artpart-office-vue/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir:'../../artpart-boot/artpartM/src/main/resources/static',
+
+  devServer: {
+    proxy: {
+      '/' : {
+        target: 'http://localhost:8282',
+        changeOrigin: true,
+        ws: false,
+      }
+    }
+  }
 })
