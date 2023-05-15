a0c53e17ce46098c6604cfca26491c5b721d92a6
SoYeoMul <128025226+SoYeoMul@users.noreply.github.com>
2023-05-15 18:27:56 +0900

    2023-05-15
    공지사항 페이징 처리

--- artpart-office-vue/src/views/notice/NoticeDetail.vue
+++ artpart-office-vue/src/views/notice/NoticeDetail.vue
@@ -1,32 +1,27 @@
 <template>
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">공지사항</h1>
-        
       </div>
-
-      
-
-      
       <div class="table-responsive">
         
         <table class="table table-striped table-sm">
           <thead class="boarder-0">
             <tr>
               <th scope style="text-align:left;">제목</th>
-              <th scope="col" style="text-align:left;" colspan="3">공지사항3</th>
+              <th scope="col" style="text-align:left;" colspan="3">{{ title }}</th>
             </tr>
             <tr>
               <th scope style="text-align:left;">작성자</th>
-              <th scope="col" style="text-align:left;" colspan="3">작성자2</th>
+              <th scope="col" style="text-align:left;" colspan="3">{{ writer }}</th>
             </tr>
             <tr>
               <th scope style="text-align:left;">작성일자</th>
-              <th scope="col" style="text-align:left;" colspan="3">2023-04-07</th>
+              <th scope="col" style="text-align:left;" colspan="3">{{ noticedate }}</th>
             </tr>
 
             <tr>
               <th scope="col" colspan="4" style="text-align:right;">
-                <router-link to="/notice-list" class="btn btn-sm btn-outline-secondary">목록</router-link>
+                  <button class="btn btn-sm btn-outline-secondary" v-on:click="fnList">목록</button>
                 <button type="button" class="btn btn-sm btn-outline-secondary">수정</button>
                 <button type="button" class="btn btn-sm btn-outline-secondary">삭제</button>
               </th>
@@ -34,10 +29,8 @@
           </thead>
           <tbody>
             <tr>
-              <td colspan="4" style="text-align:left;" >
-                  <input type="text" readonly v-model="content"><br>
-               <br><br><br><br><br><br><br><br><br>
-              </td>
+                <td colspan="4" style="text-align:left; height: 300px; width: 200px; overflow: auto;" v-text="content">
+                </td>
             </tr>
             <tr>
               <td><button type="button" class="btn btn-dark" @click="sendsms">메세지 발송</button></td>
@@ -54,7 +47,12 @@
 export default {
   data(){
       return {
-          content: '2023년 4월 27일 공지사항입니다.'
+          requestBody: this.$route.query,
+          id: this.$route.query.noticeidx,
+          title: '',
+          content: '',
+          noticedate: '',
+          writer:''
       };
   },
     methods: {
@@ -64,7 +62,30 @@ export default {
                   content: this.content
               }
           })
+      },
+      fetchNotice(){
+          this.$axios.get(this.$serverUrl + "/notice/" + this.id, {
+              params: this.requestBody
+          }).then(response => {
+                  this.title = response.data.title;
+                  this.content = response.data.content;
+                  this.noticedate = response.data.noticedate;
+                  this.writer = response.data.writer.emp_name;
+              })
+              .catch(error => {
+                  console.error(error);
+              });
+      },
+      fnList(){
+          delete this.requestBody.noticeidx
+          this.$router.push({
+              path: './list',
+              query: this.requestBody
+          })
       }
+    },
+    mounted() {
+      this.fetchNotice();
     }
 }
 </script>
