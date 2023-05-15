43fbe93c47d67a96c4b14213abf22f950c1f38fa
SongJiHyung <gud4808@gmail.com>
2023-05-15 18:21:58 +0900

    뷰페이지 수정
    뷰페이지 수정

--- artpart-vue/src/views/manageMent/UsingList.vue
+++ artpart-vue/src/views/manageMent/UsingList.vue
@@ -19,86 +19,15 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>2023-04-25</td>
-              <td>500,000</td>
-              <td>2023/04</td>
-              <td>2023-04-25</td>
-              <td>KB국민</td>
-              <td>납부완료</td>
+            <tr v-for ="(item, detailIndex) in list" :key="detailIndex">
+              <td>{{item.payment_date}}</td>
+              <td>{{item.payment_money}}</td>
+              <td>{{item.payment_date}}</td>
+              <td>{{item.payment_dad_line}}</td>
+              <td>{{item.payment_bank}}</td>
+              <td>{{item.payment_status}}</td>
             </tr>
-            <tr>
-              <td>2023-04-25</td>
-              <td>500,000</td>
-              <td>2023/04</td>
-              <td>2023-04-25</td>
-              <td>KB국민</td>
-              <td>납부완료</td>
-            </tr>
-            <tr>
-              <td>2023-04-25</td>
-              <td>500,000</td>
-              <td>2023/04</td>
-              <td>2023-04-25</td>
-              <td>KB국민</td>
-              <td>납부완료</td>
-            </tr>
-            <tr>
-              <td>2023-04-25</td>
-              <td>500,000</td>
-              <td>2023/04</td>
-              <td>2023-04-25</td>
-              <td>KB국민</td>
-              <td>납부완료</td>
-            </tr>
-            <tr>
-              <td>2023-04-25</td>
-              <td>500,000</td>
-              <td>2023/04</td>
-              <td>2023-04-25</td>
-              <td>KB국민</td>
-              <td>납부완료</td>
-            </tr>
-            <tr>
-              <td>2023-04-25</td>
-              <td>500,000</td>
-              <td>2023/04</td>
-              <td>2023-04-25</td>
-              <td>KB국민</td>
-              <td>납부완료</td>
-            </tr>
-            <tr>
-              <td>2023-04-25</td>
-              <td>500,000</td>
-              <td>2023/04</td>
-              <td>2023-04-25</td>
-              <td>KB국민</td>
-              <td>납부완료</td>
-            </tr>
-            <tr>
-              <td>2023-04-25</td>
-              <td>500,000</td>
-              <td>2023/04</td>
-              <td>2023-04-25</td>
-              <td>KB국민</td>
-              <td>납부완료</td>
-            </tr>
-            <tr>
-              <td>2023-04-25</td>
-              <td>500,000</td>
-              <td>2023/04</td>
-              <td>2023-04-25</td>
-              <td>KB국민</td>
-              <td>납부완료</td>
-            </tr>
-            <tr>
-              <td>2023-04-25</td>
-              <td>500,000</td>
-              <td>2023/04</td>
-              <td>2023-04-25</td>
-              <td>KB국민</td>
-              <td>납부완료</td>
-            </tr>
+            
 
           </tbody>
         </table>
@@ -109,8 +38,65 @@
 
 
 export default {
-  
  
+  data(){
+      return {
+          requestBody: {}, //리스트 페이지 데이터전송
+          list: {}, //리스트 데이터
+          no: '', //게시판 숫자처리
+           paging: {
+               block: 0,
+               end_page: 0,
+               next_block: 0,
+               page: 0,
+               page_size: 0,
+              prev_block: 0,
+              start_index: 0,
+              start_page: 0,
+              total_block_cnt: 0,
+              total_list_cnt: 0,
+              total_page_cnt: 0,
+          }, //페이징 데이터
+          page: this.$route.query.page ? this.$route.query.page : 1,
+          size: this.$route.query.size ? this.$route.query.size : 10,
+          //keyword: this.$route.query.keyword,
+       
+          paginavigation: function () { //페이징 처리 for문 커스텀
+              let pageNumber = [] //;
+              let start_page = this.paging.start_page;
+              let end_page = this.paging.end_page;
+              for (let i = start_page; i <= end_page; i++) pageNumber.push(i);
+              return pageNumber;
+          }
+      }
+  },
+  methods: {
+    UsingList(){
+          //스프링 부트에서 전송받은 데이터 출력 처리
+          this.requestBody = { // 데이터 전송
+
+              page: this.page,
+              size: this.size
+          }
+
+          this.$axios.get(this.$serverUrl + "/using/list", {
+              params: this.requestBody,
+              headers: {}
+          }).then((res) => {
+              //this.list = res.data  //서버에서 데이터를 목록으로 보내므로 바로 할당하여 사용할 수 있다.
+              if (res.data.result_code === "OK") {
+                  this.list = res.data.data
+                  this.paging = res.data.pagination
+                   this.no = this.paging.total_list_cnt - ((this.paging.page - 1) * this.paging.page_size)
+              }
+              }).catch( error => {
+                  console.error(error);
+              })
+      }
+  },
+ mounted(){
+  this.UsingList();
+ }
 }
 </script>
 
