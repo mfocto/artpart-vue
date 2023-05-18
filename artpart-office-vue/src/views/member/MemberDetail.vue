1a827441b9c54c41e57a6da98957acca01a8001f
kimkyunghwan1 <adamanda12345@gmail.com>
2023-05-18 18:32:27 +0900

    경환 멤버뷰
    ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- artpart-office-vue/src/views/member/MemberDetail.vue
+++ artpart-office-vue/src/views/member/MemberDetail.vue
@@ -3,11 +3,9 @@
           <h1 class="h2">입주민</h1>
           <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
-                <router-link to="/MemberUpdate">
-              <button type="button" class="btn btn-sm btn-outline-secondary">수정</button>
-            </router-link>
-              <button type="button" class="btn btn-sm btn-outline-secondary">삭제</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">목록</button>
+                <button class="btn btn-sm btn-outline-secondary" v-on:click="fnList">목록</button>
+                <button class="btn btn-sm btn-outline-secondary" v-on:click="fnUpdate">수정</button>
+                <button class="btn btn-sm btn-outline-secondary" v-on:click="fnDelete">삭제</button>
             </div>
             
           </div>
@@ -18,47 +16,42 @@
           <table class="table table-striped table-sm">
             <thead>
               <tr>      
-                <th scope="col">동</th>   <td scope="col">1001</td>           
+                <th scope="col">동</th>   <td scope="col">{{ memberdong }}</td>
               </tr>
             </thead>
             <thead>
               <tr>      
-                <th scope="col">호수</th>   <td scope="col">101</td>          
+                <th scope="col">호수</th>   <td scope="col">{{memberho}}</td>
               </tr>
             </thead>
             <thead>
               <tr>      
-                <th scope="col">입주날짜</th>   <td scope="col">2020-01-01</td>   
+                <th scope="col">핸드폰번호</th>   <td scope="col">{{ memberphone }}</td>
               </tr>
             </thead>
             <thead>
               <tr>      
-                <th scope="col">핸드폰번호</th>   <td scope="col">010-8896-4808</td>   
+                <th scope="col">등록차량1</th>   <td scope="col">{{membermycar1}}</td>
               </tr>
             </thead>
             <thead>
               <tr>      
-                <th scope="col">등록차량1</th>   <td scope="col">12하 1234</td>   
+                <th scope="col">등록차량2</th>   <td scope="col">{{membermycar2}}</td>
               </tr>
             </thead>
             <thead>
               <tr>      
-                <th scope="col">등록차량2</th>   <td scope="col">34하 5678</td>   
+                <th scope="col">아이디</th>   <td scope="col">{{memberid}}</td>
               </tr>
             </thead>
             <thead>
               <tr>      
-                <th scope="col">아이디</th>   <td scope="col">gud4808</td>   
+                <th scope="col">이름</th>   <td scope="col">{{ membername }}</td>
               </tr>
             </thead>
             <thead>
               <tr>      
-                <th scope="col">이름</th>   <td scope="col">송지형</td>   
-              </tr>
-            </thead>
-            <thead>
-              <tr>      
-                <th scope="col">입주여부</th>   <td scope="col">Y</td>   
+                <th scope="col">로그인 여부</th>   <td scope="col">{{ memberloginok }}</td>
               </tr>
             </thead>
             
@@ -68,7 +61,73 @@
   
   <script>
   export default {
-   
+      data(){
+          return {
+              requestBody: this.$route.query,
+              id: this.$route.query.memberidx,
+              memberdong : this.$route.query.memberdong,
+              memberho : this.$route.query.memberho,
+              memberid : this.$route.query.memberid,
+              memberloginok : this.$route.query.memberloginok,
+              membermycar1 : this.$route.query.membermycar1,
+              membermycar2 : this.$route.query.membermycar2,
+              membername : this.$route.query.membername,
+              memberpassword : this.$route.query.memberpassword,
+              memberphone : this.$route.query.memberphone,
+              aptidx : this.$route.query.aptidx
+
+          };
+      },
+      methods: {
+          fetchMember(){
+              this.$axios.get(this.$serverUrl + "/member/" + this.id, {
+                  params: this.requestBody
+              }).then(response => {
+                  this.memberdong = response.data.memberdong;
+                  this.memberho = response.data.memberho;
+                  this.memberid = response.data.memberid;
+                  this.memberloginok = response.data.memberloginok;
+                  this.membermycar1 = response.data.membermycar1;
+                  this.membermycar2 = response.data.membermycar2;
+                  this.membername = response.data.membername;
+                  this.memberpassword = response.data.memberpassword;
+                  this.memberphone = response.data.memberphone;
+                  this.aptidx = response.data.aptidx;
+              })
+                  .catch(error => {
+                      console.error(error);
+                  });
+          },
+          fnList(){
+              delete this.requestBody.id
+              this.$router.push({
+                  path: './memberlist',
+                  query: this.requestBody
+              })
+          },
+          fnUpdate(){
+              this.$router.push({
+                  path: './memberupdate',
+                  query: this.requestBody
+              })
+          },
+          fnDelete(){
+              if(!confirm("삭제하시겠습니까?")) return
+
+              this.$axios.delete(this.$serverUrl + '/member/' + this.id, {})
+                  .then(() => {
+                      alert('삭제되었습니다.')
+                      this.fnList();
+                  }).catch((err) => {
+                  console.log(err);
+              })
+          },
+
+      },
+      mounted() {
+          this.fetchMember();
+
+      }
   
   }
   </script>
