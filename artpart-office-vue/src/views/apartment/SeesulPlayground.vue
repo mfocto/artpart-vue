ad7a8b7308bdccc3f9b2851eb5f33f0744f14c05
SoYeoMul <128025226+SoYeoMul@users.noreply.github.com>
2023-05-24 17:16:54 +0900

    시설관리 수정
    시설관리 라디오버튼 읽기모드에서도 잘보이게 수정
    검침 셀렉트박스 날짜 추가

--- artpart-office-vue/src/views/apartment/SeesulPlayground.vue
+++ artpart-office-vue/src/views/apartment/SeesulPlayground.vue
@@ -30,40 +30,40 @@
             <tr>
               <td> 1 </td>
               <td style="text-align:left;">안전표시 및 준수사항등의 안내는 되어 있는가</td>
-              <td><input type="radio"  name="playground1result" value="Y" class="custom-input" :disabled="isReadonly" v-model="playgroundResults[0]" /> 양호
-              <input type="radio" name="playground1result" value="N" class="custom-input" :disabled="isReadonly" v-model="playgroundResults[0]" /> 불량</td>
+              <td><input type="radio"  name="playground1result" value="Y" class="form-check-input" :disabled="isReadonly" v-model="playgroundResults[0]" /> 양호
+              <input type="radio" name="playground1result" value="N" class="form-check-input" :disabled="isReadonly" v-model="playgroundResults[0]" /> 불량</td>
               <td><input type="text" :readonly="isReadonly" class="custom-input" v-model="playgroundProcess[0]" /></td>
               <td></td>
             </tr>
             <tr>
               <td> 2 </td>
               <td style="text-align:left;">놀이시설의 파손, 고장, 훼손, 너트풀림등은 없는가</td>
-              <td><input type="radio"  name="playground2result" value="Y" class="custom-input" :disabled="isReadonly" v-model="playgroundResults[1]" /> 양호
-              <input type="radio" name="playground2result" value="N" class="custom-input" :disabled="isReadonly" v-model="playgroundResults[1]" /> 불량</td>
+              <td><input type="radio"  name="playground2result" value="Y" class="form-check-input" :disabled="isReadonly" v-model="playgroundResults[1]" /> 양호
+              <input type="radio" name="playground2result" value="N" class="form-check-input" :disabled="isReadonly" v-model="playgroundResults[1]" /> 불량</td>
               <td><input type="text" :readonly="isReadonly" class="custom-input" v-model="playgroundProcess[1]" /></td>
               <td></td>
             </tr>
             <tr>
               <td> 3 </td>
               <td style="text-align:left;">놀이시설의 작동부 주유상태는 양호한가</td>
-              <td><input type="radio"  name="playground3result" value="Y" class="custom-input" :disabled="isReadonly" v-model="playgroundResults[2]" /> 양호
-              <input type="radio" name="playground3result" value="N" class="custom-input" :disabled="isReadonly" v-model="playgroundResults[2]" /> 불량</td>
+              <td><input type="radio"  name="playground3result" value="Y" class="form-check-input" :disabled="isReadonly" v-model="playgroundResults[2]" /> 양호
+              <input type="radio" name="playground3result" value="N" class="form-check-input" :disabled="isReadonly" v-model="playgroundResults[2]" /> 불량</td>
               <td><input type="text" :readonly="isReadonly" class="custom-input" v-model="playgroundProcess[2]" /></td>
               <td></td>
             </tr>
             <tr>
               <td> 4 </td>
               <td style="text-align:left;">모래바닥에 깨진 유리, 돌등은 없는가</td>
-              <td><input type="radio"  name="playground4result" value="Y" class="custom-input" :disabled="isReadonly" v-model="playgroundResults[3]" /> 양호
-              <input type="radio" name="playground4result" value="N" class="custom-input" :disabled="isReadonly" v-model="playgroundResults[3]" /> 불량</td>
+              <td><input type="radio"  name="playground4result" value="Y" class="form-check-input" :disabled="isReadonly" v-model="playgroundResults[3]" /> 양호
+              <input type="radio" name="playground4result" value="N" class="form-check-input" :disabled="isReadonly" v-model="playgroundResults[3]" /> 불량</td>
               <td><input type="text" :readonly="isReadonly" class="custom-input" v-model="playgroundProcess[3]" /></td>
               <td></td>
             </tr>
             <tr>
               <td> 5 </td>
               <td style="text-align:left;">놀이터 CCTV는 정상 작동하는가</td>
-              <td><input type="radio"  name="playground5result" value="Y" class="custom-input" :disabled="isReadonly" v-model="playgroundResults[4]" /> 양호
-              <input type="radio" name="playground5result" value="N" class="custom-input" :disabled="isReadonly" v-model="playgroundResults[4]" /> 불량</td>
+              <td><input type="radio"  name="playground5result" value="Y" class="form-check-input" :disabled="isReadonly" v-model="playgroundResults[4]" /> 양호
+              <input type="radio" name="playground5result" value="N" class="form-check-input" :disabled="isReadonly" v-model="playgroundResults[4]" /> 불량</td>
               <td><input type="text" :readonly="isReadonly" class="custom-input" v-model="playgroundProcess[4]" /></td>
               <td></td>
             </tr>
@@ -230,4 +230,8 @@ a {
     background: transparent;
     text-align: center;
   }
+.radio-readonly {
+    /* 읽기 전용 상태일 때 색상 */
+    background: blue;
+}
 </style>
